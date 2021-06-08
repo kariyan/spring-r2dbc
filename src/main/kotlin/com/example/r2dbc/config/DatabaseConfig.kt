71b7e396b94c64e8a73de0df95f4b8b5ae4c31b6
김은택 <etkim@wemakeprice.com>
2021-06-09 00:00:24 +0900

    use R2DBC java configuration

---
+++ src/main/kotlin/com/example/r2dbc/config/DatabaseConfig.kt
@@ -0,0 +1,30 @@
+package com.example.r2dbc.config
+
+import io.r2dbc.spi.ConnectionFactories
+import io.r2dbc.spi.ConnectionFactory
+import io.r2dbc.spi.ConnectionFactoryOptions
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+
+@Configuration
+@EnableR2dbcRepositories
+class DatabaseConfig : AbstractR2dbcConfiguration() {
+    @Bean
+    override fun connectionFactory(): ConnectionFactory {
+        val userName = "grp_travel_dev"
+        val password = "NjI3ZDUwNzM5N2Jj"
+        return ConnectionFactories.get(
+            ConnectionFactoryOptions.builder()
+                .option(ConnectionFactoryOptions.DRIVER, "mysql")
+                .option(ConnectionFactoryOptions.PROTOCOL, "r2dbc")
+                .option(ConnectionFactoryOptions.HOST, "nd-integrated.devdb.wemakeprice.work")
+                .option(ConnectionFactoryOptions.PORT, 3307)
+                .option(ConnectionFactoryOptions.DATABASE, "hms_dev")
+                .option(ConnectionFactoryOptions.USER, userName)
+                .option(ConnectionFactoryOptions.PASSWORD, password)
+                .build()
+        )
+    }
+}
